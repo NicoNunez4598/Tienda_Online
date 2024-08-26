feaca039aff0992928bcd51c1dc9a5717a9708ca
José Nicolás Núñez <nunezjosenicolas4598@gmail.com>
2024-08-26 18:43:00 -0300

    mejoras varias
    mejoras varias

--- Tienda_Online/GestiondePedidos/views.py
+++ Tienda_Online/GestiondePedidos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def busqueda(request):
+    
+    return render(request, 'Busqueda.html')
+
+def buscar(request):
+
+    mensaje = "Articulo Buscado: %r" %request.GET["prd"]
+
+    return HttpResponse(mensaje)
--- Tienda_Online/Tienda_Online/urls.py
+++ Tienda_Online/Tienda_Online/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from GestiondePedidos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Busqueda/', views.busqueda),
+    path('buscar/', views.buscar),
 ]
